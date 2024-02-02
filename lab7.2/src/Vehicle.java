5ff668962d2f6b321ed929d96312f062b435f7fc
Thxnxt <141336398+Thxnxt@users.noreply.github.com>
2024-02-02 20:13:33 +0700

    Update Vehicle.java

--- lab7.2/src/Vehicle.java
+++ lab7.2/src/Vehicle.java
@@ -1,14 +1,17 @@
-
 public abstract class Vehicle implements Dieselable{
     protected double fuel;
     public Vehicle(){
-        
+        this(0);
     }
     public Vehicle(double fuel){
-        
+        this.fuel = fuel;
     }
     public void addFuel(double fuel){
-        
+        if (fuel > 0) {
+            setFuel(getFuel()+fuel);
+        }else{
+            System.out.println("Fuel is empty.");
+        }
     }
     public void setFuel(double fuel){
         this.fuel = fuel;
